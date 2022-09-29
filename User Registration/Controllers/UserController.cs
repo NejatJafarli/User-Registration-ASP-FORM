75d06c97399c123edaa0a6daca6ca422fec2fb56
Nejat Jafarligmail.co <Mega.ceferli@gmail.com>
2022-09-30 00:36:21 +0400

    Update

--- User Registration/Controllers/UserController.cs
+++ User Registration/Controllers/UserController.cs
@@ -7,11 +7,19 @@ using System.Reflection;
 using User_Registration.Entities;
 using User_Registration.Models;
 using User_Registration.Services;
+using System.IO;
+using Microsoft.AspNetCore.Http;
 
 namespace User_Registration.Controllers
 {
     public class UserController : Controller
     {
+
+        private readonly IHostingEnvironment Environment;
+        public UserController(IHostingEnvironment _environment)
+        {
+            Environment = _environment;
+        }
         public IActionResult Index()
         {
             return View(UserService.Users);
@@ -26,7 +34,28 @@ namespace User_Registration.Controllers
         [HttpPost]
         public IActionResult Add(UserAddViewModel userAdd)
         {
-            
+            string wwwPath = this.Environment.WebRootPath;
+            string contentPath = this.Environment.ContentRootPath;
+
+            string path = Path.Combine(this.Environment.WebRootPath, "Uploads");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+
+            string fileName = Path.GetFileName(userAdd.File.FileName);
+            using (FileStream stream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
+            {
+                userAdd.File.CopyTo(stream);
+            }
+
+            userAdd.User.ImagePath = fileName;
+
+            UserService.Users.Add(userAdd.User);
+
+            return RedirectToAction("Index");
+
         }
     }
 }
